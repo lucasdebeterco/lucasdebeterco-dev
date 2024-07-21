e675cfc1cb9a9e2c2c2cb46f03fcd28a9b58f13b
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-07-21 18:16:20 -0300

    About presentation area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,17 @@ export default function Home() {
         <>
             <Header />
 
-            <div className="pt-28">
-                <section id="about-section" className="h-screen">
-                    About
+            <div className="mx-auto max-w-[1360px] px-10 pt-28">
+                <section id="about-section" className="flex h-[80vh] flex-col justify-center gap-6">
+                    <span className="text-purple-600">Hi, my name is</span>
+                    <div className="flex flex-col gap-2 font-bold">
+                        <h1 className="text-6xl text-purple-600">Lucas Debeterco.</h1>
+                        <h2 className="max-w-[60%] text-3xl">Passionate about creating web solutions for modern challanges.</h2>
+                    </div>
+                    <span className="max-w-[60%] text-zinc-400">
+                        I’m a front-end software engineer specialized in building quality digital experiences.
+                        Currently, I’m focused on building accessible products using the <span className="font-bold">Javascript ecosystem</span>
+                    </span>
                 </section>
                 <section id="experience-section" className="h-screen">
                     Experience
--- src/app/utils/scrollSmoothTo.ts
+++ src/app/utils/scrollSmoothTo.ts
@@ -1,9 +1,6 @@
 export function scrollSmoothTo(event, elementId) {
     event.preventDefault()
     const element = document.getElementById(elementId)
-
-    element.scrollIntoView({
-        block: 'start',
-        behavior: 'smooth'
-    })
+    const y = element.getBoundingClientRect().top + window.scrollY - 120
+    window.scrollTo({top: y, behavior: 'smooth'})
 }
