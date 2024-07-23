79a415e8e2c4a1f6c01780b2aa35756f8ed1aa1f
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-07-23 18:21:15 -0300

    function fix

--- src/app/utils/scrollSmoothTo.ts
+++ src/app/utils/scrollSmoothTo.ts
@@ -1,6 +1,8 @@
 export function scrollSmoothTo(event: React.MouseEvent<HTMLElement>, elementId: string) {
     event.preventDefault()
     const element = document.getElementById(elementId)
-    const y = element.getBoundingClientRect().top + window.scrollY - 120
-    window.scrollTo({top: y, behavior: 'smooth'})
+    if (element) {
+        const y = element.getBoundingClientRect().top + window.scrollY - 120
+        window.scrollTo({top: y, behavior: 'smooth'})
+    }
 }
