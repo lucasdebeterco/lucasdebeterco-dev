543b58c1648d092213e670accc9e8afcbd441088
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2025-01-21 00:27:53 -0300

    eslint config

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin, Twitter, Mail, Heart } from 'lucide-react';
+import { Github, Linkedin, Twitter, Mail } from 'lucide-react';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
